094ad0a4e86edc3b94d9b7ff742e876b29e01d46
Hector Torrez <69362017+HectorTorrez@users.noreply.github.com>
2022-02-07 22:19:53 -0600

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <firstcomponent/>
+  </div>
+</template>
+
+<script>
+
+// import HelloWorld from './components/HelloWorld.vue'\
+import firstcomponent from './components/firstcomponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    firstcomponent
+    // HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ firstcomponent.vue
@@ -0,0 +1,62 @@
+<template>
+    <div id="root">
+        <h1>{{ title }}</h1>
+
+        Precione una tecla
+        <input type="text" v-on:keydown="key" >
+        <!-- <p>Tecla presionada {{keycode}}</p>  -->
+        <p v-show="resultado() " :style="{color: colorgreen}">Tecla presionada {{keycode}}</p>
+        <p v-show="!resultado() " :style="{color: colorred}">Tecla presionada {{keycode}}</p>
+        <p v-show="resultado() " :style="{color: coloryellow}">Tecla presionada {{keycode}}</p>
+
+        <!-- no pude llamar de otra manera a la ultima condicion -->
+    </div>
+</template>
+
+
+<script>
+export default {
+     name:"firstcomponent",
+     data(){
+         return{
+             title:"Mi primer componente",
+             keycode:'',
+             colorgreen:'green',
+             colorred:'red',
+             coloryellow:"yellow"
+
+
+         }
+     },
+     methods:{
+
+         resultado(){
+             if (this.keycode >=65 & this.keycode <=90) {
+                 return true
+             } else if (this.keycode >=48 & this.keycode<=57) {
+                    return false  
+             } else {
+                return  
+             } 
+                 
+             
+         },
+
+         key(event){
+             this.keycode= event.keyCode
+           
+         },
+
+         computed:{
+             styleP(){
+                 return{
+                     color: 'blue'
+                 }
+             }
+         }
+
+
+     }
+    
+}
+</script>
